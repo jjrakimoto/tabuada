a530d8549973587e29e60cfdfa0a7a24b912aaa4
matheus.salves14 <matheus.salves14@SBC0505222W10-1>
2021-09-27 11:46:18 -0300

    Adicionar arquivos de projeto.

---
+++ tabuada/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace tabuada
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtnum = new System.Windows.Forms.TextBox();
+            this.lboxresul = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Maroon;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(335, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tabuada";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Ravie", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(265, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Número:";
+            // 
+            // txtnum
+            // 
+            this.txtnum.Location = new System.Drawing.Point(343, 115);
+            this.txtnum.Name = "txtnum";
+            this.txtnum.Size = new System.Drawing.Size(100, 23);
+            this.txtnum.TabIndex = 2;
+            this.txtnum.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtnum_KeyPress);
+            // 
+            // lboxresul
+            // 
+            this.lboxresul.BackColor = System.Drawing.Color.White;
+            this.lboxresul.FormattingEnabled = true;
+            this.lboxresul.ItemHeight = 15;
+            this.lboxresul.Location = new System.Drawing.Point(262, 196);
+            this.lboxresul.Name = "lboxresul";
+            this.lboxresul.Size = new System.Drawing.Size(231, 214);
+            this.lboxresul.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkRed;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lboxresul);
+            this.Controls.Add(this.txtnum);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtnum;
+        private System.Windows.Forms.ListBox lboxresul;
+    }
+}
+
---
+++ tabuada/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tabuada
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void CalcularTabuada()
+        {
+            lboxresul.Items.Clear();
+          
+            int valor = Convert.ToInt32(txtnum.Text);
+            for (int i = 0; i <= 10; i++)
+            {
+                lboxresul.Items.Add(valor * i);
+            }
+         }
+    private void txtnum_KeyPress(object sender, KeyPressEventArgs e)
+    {
+        if (e.KeyChar >= '0' && e.KeyChar <= '9' || e.KeyChar == '\b' || e.KeyChar == '\r')
+        {
+            if (e.KeyChar == '\r')
+            {
+                CalcularTabuada();
+
+            }
+                txtnum.Focus();
+                txtnum.Clear();
+        }
+        else
+            {
+                e.KeyChar = '\0';
+
+            }
+    }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
+
+    
